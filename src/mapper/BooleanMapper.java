d0a6b28769cce214ba195a0585ea8b59855a39bb
Kutep85 <genik_630@i.ua>
2017-07-22 15:38:26 +0300

    Update BooleanMapper.java

--- src/mapper/BooleanMapper.java
+++ src/mapper/BooleanMapper.java
@@ -5,6 +5,8 @@ import writer.JsonWriter;
 public class BooleanMapper implements JsonMapper{
     @Override
     public void write(Object obj, JsonWriter writer) {
-
+	
+	Boolean bool = (Boolean) obj;
+	writer.writeBoolean(bool);
     }
 }
